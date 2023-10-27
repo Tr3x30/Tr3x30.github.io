33b77715f27d7e8c9432adca6553bbc2f09418cb
Tr3x30 <63154712+Tr3x30@users.noreply.github.com>
2023-10-27 12:13:11 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,81 @@
-// Get a reference to the trapezoid element
-const trapezoid = document.querySelector(".trapezoid");
+const wrapper = document.getElementById("tiles");
 
-// Function to move the trapezoid
-function moveTrapezoid() {
-  // Get the current scroll position
-  const scrollPosition = window.scrollY;
+let columns = 0,
+    rows = 0,
+    toggled = false;
 
-  // Calculate the vertical movement
-  const verticalMovement = scrollPosition / 2; // Adjust the factor as needed
-
-  // Apply the transform to move the trapezoid
-  trapezoid.style.transform = `translateY(${verticalMovement}px)`;
+const toggle = () => {
+  toggled = !toggled;
+  
+  document.body.classList.toggle("toggled");
 }
 
-// Add an event listener for scroll events
-window.addEventListener("scroll", moveTrapezoid);
+const handleOnClick = index => {
+  toggle();
+  
+  anime({
+    targets: ".tile",
+    opacity: toggled ? 0 : 1,
+    delay: anime.stagger(50, {
+      grid: [columns, rows],
+      from: index
+    })
+  });
+}
+
+const createTile = index => {
+  const tile = document.createElement("div");
+  
+  tile.classList.add("tile");
+  
+  tile.style.opacity = toggled ? 0 : 1;
+  
+  tile.onclick = e => handleOnClick(index);
+  
+  return tile;
+}
+
+const createTiles = quantity => {
+  Array.from(Array(quantity)).map((tile, index) => {
+    wrapper.appendChild(createTile(index));
+  });
+}
+
+const createGrid = () => {
+  wrapper.innerHTML = "";
+  
+  const size = document.body.clientWidth > 800 ? 100 : 50;
+  
+  columns = Math.floor(document.body.clientWidth / size);
+  rows = Math.floor(document.body.clientHeight / size);
+  
+  wrapper.style.setProperty("--columns", columns);
+  wrapper.style.setProperty("--rows", rows);
+  
+  createTiles(columns * rows);
+}
+
+createGrid();
+
+window.onresize = () => createGrid();
+
+const outerCursor = document.getElementById("customCursorCircle");
+const dotCursor = document.getElementById("customCursorDot");
+const customCursorText = document.getElementById("customCursorText")
+
+window.onpointermove = event => { 
+  const { clientX, clientY } = event;
+  
+  outerCursor.animate({
+    left: `${clientX}px`,
+    top: `${clientY}px`
+  }, { duration: 125, fill: "forwards" });
+  
+  customCursorText.animate({
+    left: `${clientX}px`,
+    top: `${clientY+40}px`
+  }, { duration: 125, fill: "forwards" })
+  
+  dotCursor.style.left = `${clientX}px`;
+  dotCursor.style.top = `${clientY}px`;
+}
